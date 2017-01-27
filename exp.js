d66c6cb4e7a8679628cdbbdf4298c41afa23c822
Daniel Rivas <rivasdaniel1992@gmail.com>
2017-01-27 13:23:14 -0500

    on a trouve les bons sons pour une diff

---
+++ exp.js
@@ -0,0 +1,55 @@
+function runExperiment(){
+	
+	serverPsych.request(function (settings){
+			    
+		settings.timeline.forEach(function(block, idx, timeline){
+			if(block.name == "SJ1"){
+				block.timeline = []
+				{
+					stimuli: ["", ""]
+						
+				}
+				
+				
+				function findSounds(sons, diff){
+					var bonsons=[];
+					//remplir bonsons!
+					sons.forEach(function (nom, idx){
+						if( nom.includes("_"+diff+"_") ){
+							bonsons.push(nom);
+						}
+					});
+					return bonsons
+				}
+				
+				var candidats = findSounds(settings.other.resources, settings.extra_parameters.difficulty);
+				
+				for (var i=0; i < block.length; i++)
+		    	{
+					var trial= {}
+					trial.stimuli=[]
+					//bien remplir trial.stimuli...
+					
+					
+					block.timeline.push(trial)
+		    	}
+				
+				
+			}
+			
+		});
+				
+		jsPsych.init({
+			timeline: settings.timeline,
+			on_finish:function(data){
+				serverPsych.save({
+					data:data
+				})
+			},
+			display_element: $('#jsPsychTarget'),
+			on_trial_start:function(){
+				$("#jsPsychTarget")[0].scrollIntoView();
+			}
+		});
+	});
+}
